99b14927aa1f0636a177ebfc95637787a2b8109b
jayt9 <jthairu@icloud.com>
2025-02-16 10:51:47 -0500

    normalize function

---
+++ gso.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+//Return unit vector 
+vector<float> normalize(vector<float> a) {
+    vector<float> b;
+    float length;
+    for (int i=0;i<a.size();i++) {
+        length += (a[i]*a[i]);
+    };
+    length = sqrt(length);
+    for (int i=0;i<a.size();i++) {
+        b[i] = (a[i]/length);
+    };
+    return b;
+};
